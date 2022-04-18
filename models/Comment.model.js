023e3377aa4077740d9fa638f3d4350426ed6e85
Abubakar <aozniev98@.mail.ru>
2022-04-18 23:33:49 +0300

    Abubakar

--- models/Comment.model.js
+++ models/Comment.model.js
@@ -1,1 +1,20 @@
-// БАКАР
+const mongoose = require("mongoose");
+
+const commentSchema = mongoose.Schema({
+    text: String,
+    user: {
+        ref: "User",
+        type: mongoose.Schema.Types.ObjectId,
+    },
+    books: {
+        ref: "book",
+        type: mongoose.Schema.Types.ObjectId,
+    },
+    date: Number,
+    author: String,
+    required: true,
+});
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+module.exports = Comment;
