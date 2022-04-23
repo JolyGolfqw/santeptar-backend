6ef02135d644ee9a2c530a15172dd5abe877a947
sardalov <sarda10v@bk.ru>
2022-04-23 19:57:38 +0300

    save

--- models/User.model.js
+++ models/User.model.js
@@ -1,22 +1,25 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  avatar: String,
-  name: {type: String, required: true},
+  avatar: {
+    type: String,
+    default:
+      "images\\kisspng-avatar-user-computer-icons-software-developer-5b327cc98b5780.5684824215300354015708.jpg",
+  },
+  name: { type: String, required: true },
   login: {
     type: String,
     unique: true,
   },
 
   description: String,
-  
 
   password: {
     type: String,
     required: true,
   },
 
-  followers: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 });
 
 const User = mongoose.model("User", userSchema);
